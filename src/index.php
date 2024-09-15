88cd1a3867fd72b5f81cfeee44a3c0d3c0ffb244
KLyKir <82658429+KLyKir@users.noreply.github.com>
2024-09-15 15:50:12 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,12 +1,13 @@
 <?php
 require 'test1.php';
 require 'test2.php';
-echo test1::last_word("")."<br/>";
-echo test1::extract_string("The quick [brown fox] [dark horse].")."<br/>";
+echo "Task 1". "<br/>";
+echo "Number of last word: " . test1::last_word("Hello world")."<br/>";
+echo "Extract string: " . test1::extract_string("The quick [brown fox] [dark horse].")."<br/>";
+echo "Task 2" . "<br/>";
 $test2 = new test2();
-//echo $test2->is_power(1.4884, 1.22);
-echo $test2->is_power(2, 100)."<br/>";
-echo $test2->format_number("o874.5pd&*2,")."<br/>";
-echo $test2->sum_digits("o874.5pd&*2")."<br/>";
-echo $test2->sum_digits("o");
+echo "Is power: " . $test2->is_power(2, 100)."<br/>";
+echo "Format number: " . $test2->format_number("o874.5pd&*2,")."<br/>";
+echo "Sum of digits: " . $test2->sum_digits("o874.5pd&*2")."<br/>";
 ?>
+
